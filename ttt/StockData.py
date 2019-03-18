85a0e31a2d38425903e758710465c046612050c4
Antoni Chrobot <antoni.chrobot@gmail.com>
2019-03-18 18:58:26 -0400

    Got an interactive plot to appear on home view -Antoni

--- ttt/StockData.py
+++ ttt/StockData.py
@@ -1,10 +1,3 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim:fenc=utf-8
-#
-# Copyright © 2019 antoni <antoni@antoni>
-#
-# Distributed under terms of the MIT license.
 
 api_key = 'B9KY7ZHBX1HNKGHO'
 
@@ -22,43 +15,6 @@ class StockData():
     def getValues():
         ts = TimeSeries(key=api_key, output_format='pandas')
         data, meta_data = ts.get_weekly(symbol='MSFT')
-        x=data.index.values
+        x=data.index.values # get index values (dates)
         y=data.iloc[:,3]    # get third column which is closing price
         return x,y
-        
-    @staticmethod
-    def getX():
-        return [0,1,2,3,4]
-
-    @staticmethod
-    def getY():
-        return [0,1,4,9,16]
-
-
-    @staticmethod
-    def getPlot():
-        ts = TimeSeries(key=api_key, output_format='pandas')
-        data, meta_data = ts.get_weekly(symbol='MSFT')
-
-'''
-ts = TimeSeries(key=api_key, output_format='pandas')
-cc = CryptoCurrencies(key=api_key, output_format='pandas')
-
-#data, meta_data = ts.get_intraday(symbol='MPGFX', interval='5min',outputsize='full')
-data_BTC, meta_data_BTC = cc.get_digital_currency_weekly(symbol='BTC', market='USD')
-
-# plot data at closing
-plt.subplot(211)
-data_MPFGX['4. close'].plot()
-plt.title('MPGFX weekly performance')
-
-# plot data at closing
-plt.subplot(212)
-data_BTC['4a. close (USD)'].plot()
-plt.title('BTC weekly performance')
-
-# get 20 most recent values
-# last_20_MPGFX = data.iloc[-20:-1, :]
-
-plt.show()
-'''
