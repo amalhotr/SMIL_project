02fc62ab778ad4bac6355c088fe2a366a73d5f87
Antoni Chrobot <antoni.chrobot@gmail.com>
2019-03-18 12:02:06 -0400

    Added views for home, dashboard, trade. html templates found under templates/ - Antoni

--- ttt/urls.py
+++ ttt/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import *
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('dashboard/', DashboardPageView.as_view(), name='dashboard'),
+    path('trade/', TradePageView.as_view(), name='trade'),
 ]
