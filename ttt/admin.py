6c4046433d0c0872fd7d6acfe479c61854509d88
Albert Joshua Capistrano <43123388+joshcapistrano@users.noreply.github.com>
2019-03-18 15:30:11 -0400

    Added league, transactions, and pending models
    .

--- ttt/admin.py
+++ ttt/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import League
+from .models import Transactions
+
+admin.site.register(League)
+admin.site.register(Transactions)
