fed91bac04aa35daf6d195d786759ec4c6955a51
Antoni Chrobot <antoni.chrobot@gmail.com>
2019-03-22 12:25:04 -0400

    added some more unit tests -Antoni

--- ttt/StockData.py
+++ ttt/StockData.py
@@ -1,9 +1,8 @@
 
 api_key = 'B9KY7ZHBX1HNKGHO'
-
-
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.cryptocurrencies import CryptoCurrencies
+
 import pandas
 
 class StockData():
@@ -36,11 +35,13 @@ class StockData():
     def getCurrentPrice(symbol, currency='stock'):
         ''' get current price for a given stock or cryptocurrency
             param symbol: string of the desired stock or cryptocurrency
-            param currency: 'stock' or 'crypto' '''
+            param currency: 'stock' or 'crypto' 
+            return tuple(x,y) x is string with date and time
+                                y is the stock/crypto value '''
         if currency=='stock':
             ts = TimeSeries(key=api_key, output_format='pandas')
             try:
-                data, meta_data = ts.get_intraday(symbol, interval='1min', outputsize='compact')
+                data, meta_data = ts.get_weekly(symbol)
             except KeyError:
                 return None, None
             x=data.index.values[-1] # get index values (dates)
@@ -49,10 +50,9 @@ class StockData():
         elif currency=='crypto':
             cc = CryptoCurrencies(key=api_key, output_format='pandas')
             try:
-                data, meta_data = cc.get_digital_currency_daily(symbol, market='USD')
+                data, meta_data = cc.get_digital_currency_weekly(symbol, market='USD')
             except KeyError:
                 return None, None
             x=data.index.values[-1] # get index values (dates)
             y=data.iloc[-1,3]    # get third column which is closing price
             return x,y
-
