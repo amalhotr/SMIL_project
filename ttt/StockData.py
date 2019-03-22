09a8f6ee7a9a35a8384360776e1e00318ef9daef
amalhotr <41587195+amalhotr@users.noreply.github.com>
2019-03-22 12:53:05 -0400

    Prelim Branch - Ajeet

--- ttt/StockData.py
+++ ttt/StockData.py
@@ -1,15 +1,17 @@
 
 api_key = 'B9KY7ZHBX1HNKGHO'
-
-
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.cryptocurrencies import CryptoCurrencies
+
 import pandas
 
 class StockData():
 
     @staticmethod
     def getValues(symbol, currency='stock'):
+        ''' get the full time series from a given stock or cryptocurrency
+            param symbol: symbol of desired stock or crypto
+            param currency: 'stock' or 'crypto' '''
         if currency=='stock':
             ts = TimeSeries(key=api_key, output_format='pandas')
             try:
@@ -28,18 +30,18 @@ class StockData():
             x=data.index.values # get index values (dates)
             y=data.iloc[:,3]    # get third column which is closing price
             return x,y
-<<<<<<< HEAD
-=======
 
     @staticmethod
     def getCurrentPrice(symbol, currency='stock'):
         ''' get current price for a given stock or cryptocurrency
             param symbol: string of the desired stock or cryptocurrency
-            param currency: 'stock' or 'crypto' '''
+            param currency: 'stock' or 'crypto' 
+            return tuple(x,y) x is string with date and time
+                                y is the stock/crypto value '''
         if currency=='stock':
             ts = TimeSeries(key=api_key, output_format='pandas')
             try:
-                data, meta_data = ts.get_intraday(symbol, interval='1min', outputsize='compact')
+                data, meta_data = ts.get_weekly(symbol)
             except KeyError:
                 return None, None
             x=data.index.values[-1] # get index values (dates)
@@ -48,11 +50,9 @@ class StockData():
         elif currency=='crypto':
             cc = CryptoCurrencies(key=api_key, output_format='pandas')
             try:
-                data, meta_data = cc.get_digital_currency_daily(symbol, market='USD')
+                data, meta_data = cc.get_digital_currency_weekly(symbol, market='USD')
             except KeyError:
                 return None, None
             x=data.index.values[-1] # get index values (dates)
             y=data.iloc[-1,3]    # get third column which is closing price
             return x,y
-
->>>>>>> parent of fed91ba... added some more unit tests -Antoni
