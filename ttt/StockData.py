462561d0c4ec67b1b0b41b831f63aa717f1bb108
Antoni Chrobot <antoni.chrobot@gmail.com>
2019-03-22 10:41:19 -0400

    Updated dependencies (added alpha_vantage to Pipfile - Antoni

--- ttt/StockData.py
+++ ttt/StockData.py
@@ -10,6 +10,9 @@ class StockData():
 
     @staticmethod
     def getValues(symbol, currency='stock'):
+        ''' get the full time series from a given stock or cryptocurrency
+            param symbol: symbol of desired stock or crypto
+            param currency: 'stock' or 'crypto' '''
         if currency=='stock':
             ts = TimeSeries(key=api_key, output_format='pandas')
             try:
@@ -28,3 +31,28 @@ class StockData():
             x=data.index.values # get index values (dates)
             y=data.iloc[:,3]    # get third column which is closing price
             return x,y
+
+    @staticmethod
+    def getCurrentPrice(symbol, currency='stock'):
+        ''' get current price for a given stock or cryptocurrency
+            param symbol: string of the desired stock or cryptocurrency
+            param currency: 'stock' or 'crypto' '''
+        if currency=='stock':
+            ts = TimeSeries(key=api_key, output_format='pandas')
+            try:
+                data, meta_data = ts.get_intraday(symbol, interval='1min', outputsize='compact')
+            except KeyError:
+                return None, None
+            x=data.index.values[-1] # get index values (dates)
+            y=data.iloc[-1,3]    # get third column which is closing price
+            return x,y
+        elif currency=='crypto':
+            cc = CryptoCurrencies(key=api_key, output_format='pandas')
+            try:
+                data, meta_data = cc.get_digital_currency_daily(symbol, market='USD')
+            except KeyError:
+                return None, None
+            x=data.index.values[-1] # get index values (dates)
+            y=data.iloc[-1,3]    # get third column which is closing price
+            return x,y
+
