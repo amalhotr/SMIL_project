8a5f76a24adcced74ef6c8b6d971d765e1441eb7
Albert Joshua Capistrano <43123388+joshcapistrano@users.noreply.github.com>
2019-03-22 13:07:27 -0400

    New Version
    by josh

--- ttt/urls.py
+++ ttt/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('dashboard/', DashboardPageView.as_view(), name='dashboard'),
-    path('trade/', TradePageView.as_view(), name='trade'),
+    path('', views.HomePageView.as_view(), name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/<str:league>', views.dashboardLeague, name='ticker'),
+    path('leagues/', views.leagues, name='leagues'),
+    path('trade/', views.trade, name='trade'),
+    path('trade/ticker/<str:ticker>', views.ticker, name='ticker'),
+    path('leagues/create', views.createLeague, name='createLeague'),
+    path('leagues/join/<str:leagueName>', views.joinLeague, name='joinLeague'),
+    
 ]
